e254ff16d120131607eb5502dcdfa4686d67b488
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2025-01-06 20:56:11 +0800

    ↓ optimized code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,45 +7,18 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-  final Color bg = const Color(0xFF530198);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: bg,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 275,
-              ),
-              const SizedBox(height: 70,),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 40,),
-              OutlinedButton(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  shape: const BeveledRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.white,
-                      width: 10
-                    )
-                  ),
-                ),
-                child: const Text('Start Quiz'),
-              )
-            ],
-          ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.green])),
+          child: const StartScreen(),
         ),
       ),
     );
