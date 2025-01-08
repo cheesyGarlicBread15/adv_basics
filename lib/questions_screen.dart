ca782148a5fff9c613025827e2ab796bee112bea
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2025-01-08 22:12:39 +0800

    (67) questions screen design

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,13 +12,58 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Questions Screen',
-        style: TextStyle(
-          color: Colors.red,
-          fontSize: 30,
-        ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'question here',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black45),
+              child: const Text(
+                'answer 1',
+                style: TextStyle(color: Colors.white),
+              )),
+          const SizedBox(
+            height: 6,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black45),
+              child: const Text(
+                'answer 2',
+                style: TextStyle(color: Colors.white),
+              )),
+          const SizedBox(
+            height: 6,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black45),
+              child: const Text(
+                'answer 3',
+                style: TextStyle(color: Colors.white),
+              )),
+          const SizedBox(
+            height: 6,
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black45),
+              child: const Text(
+                'answer 4',
+                style: TextStyle(color: Colors.white),
+              )),
+        ],
       ),
     );
   }
